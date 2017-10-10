c1526d84bb0e5d8b1903eb14e02e9d6492ace02b
DimitriosHytiroglou <DimitriosHytiroglou@users.noreply.github.com>
2017-10-09 19:50:01 -0700

    cleaned TwilioCall.py

--- main/TwilioCall.py
+++ main/TwilioCall.py
@@ -22,22 +22,6 @@ account_sid = "AC9629dc758c9d995440c7b90b5542d86c"
 auth_token  = "b13ff78cc4235ddbabcf41decd4269ee"
 client = Client(account_sid, auth_token)
 
-# Check rate function
-def check_rate(coin1, coin2, ex_rate, user_signal, direction, first_name, telephone, ):
-
-	if direction == 'H':
-		if ex_rate <= user_signal:
-			way = "above "
-			send_er_notification(coin1, coin2, user_signal, first_name, telephone, way)
-		else:
-			pass
-
-	elif direction == 'L':
-		if ex_rate >= user_signal:
-			way = "below "
-			send_er_notification(coin1, coin2, user_signal, first_name, telephone, way)
-		else:
-			pass
 
 # Send exchange rate notification function
 def send_er_notification(coin1, coin2, user_signal, first_name, telephone):
@@ -52,34 +36,6 @@ def send_er_notification(coin1, coin2, user_signal, first_name, telephone):
 	print("Sent: ",message.sid)
 
 
-
-# Check rate function
-#def check_rate():
-#	while exchange_rate <= 1.1:
-#		pass
-#	else:
-#		send_er_notification()
-
-# Send exchange rate notification function
-#def send_er_notification():
-#	message = client.messages.create(
-#	    to = surya_number,
-#	    from_ = "+17797747983",   # Account Twilio Number
-#	    body = ("Hey, " + user1 +": the exchange rate between " +
-#	    		str(coin_pair[0]) + " and " + str(coin_pair[1]) +
-#	    		" just crossed " + str(exchange_rate)
-#	    		+ "! " + "TO HODL OR NOT TO HODL???"))
-#
-#	print("Sent: ",message.sid)
-
-
-# Check pivot function
-def check_pivot():
-	while price > pivot_point:
-		pass
-	else:
-		send_pivot_notification()
-
 # Send pivot notification function
 def send_pivot_notification():
 	message = client.messages.create(
@@ -91,11 +47,3 @@ def send_pivot_notification():
 	    		+ "TO HODL OR NOT TO HODL???"))
 
 	print("Sent: ",message.sid)
-
-
-# Main function
-def main():
-	check_rate()
-	check_pivot()
-
-#main()
